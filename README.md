31af02df6e5a1296a611a3ede4f0f68d5f064e6f
AlvaroLuken <al5031@nyu.edu>
2021-08-19 08:19:16 -0700

    setup for bootcamp

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ProblemSet0
 Set up of ChainShot dev environment!
+
+Run `node -v` to verify your Node.js version.
+
+Run `node verifyNodeInstallation` to run a script using Node.js!
---
+++ verifyNodeInstallation.js
@@ -0,0 +1,14 @@
+console.log();
+
+console.log(`%c ________________________________________
+< Mooooo, you did it - you successfully git cloned a Github repository and used Node.js to run a script! >
+ ----------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`, "font-family:monospace");
+
+console.log();
+console.log("Great job installing Git and Node.js!");
+console.log("Now you can move on to Task 5! :)");
